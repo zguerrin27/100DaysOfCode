327e5d0b63755d34fbad61ec8f5a87379a7e37b8
Zachary Guerrin <z.guerrin@gmail.com>
2020-10-31 12:24:09 -0400

    Day3 of 100days of code

--- SubarraySumEqualsK.js
+++ SubarraySumEqualsK.js
@@ -3,6 +3,22 @@
 // Given an array of integers nums and an integer k, return the total number of continuous subarrays whose sum equals to k.
 // [1,1,1], 2
 
+// O (n^) 
+
+var subarraySum = function(nums, k) {
+  let count = 0;
+    for(let i = 0; i < nums.length; i++){
+        let sum = 0;
+        for(let j = i; j < nums.length; j++){  // loops over each value from start and totals them up
+            sum += nums[j]
+            if(sum === k){
+                count += 1;
+            }
+        }
+    }
+    return count;
+};
+
 var subarraySum = function(nums, k) {
   const map = new Map([[0, 1]]);
   let sum = 0;
