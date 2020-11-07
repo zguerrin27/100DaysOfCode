f2b712831c3a6c96bf27457ce996aede9ea16f27
Zachary Guerrin <z.guerrin@gmail.com>
2020-11-07 12:29:13 -0500

    Day8

--- String.MakingAnagrams.js
+++ String.MakingAnagrams.js
@@ -18,3 +18,32 @@ function makeAnagram(a, b) {
 
 console.log(makeAnagram(firstString, secondString))
 
+
+function makeAnagram(a, b) {
+  const larger = a.length >= b.length ? a : b;
+  const smaller = a.length < b.length ? a : b;
+
+  //store character counts for larger string 
+  const storage = {};
+  for(const letter of larger){
+    storage[letter] = (storage[letter] || 0) + 1;
+  }
+
+  //init counter for similar characters
+  let counter = 0;
+  //loop through second/smaller string to find count of similar characters 
+  for(const letter of smaller){
+    if(storage[letter] && storage[letter] > 0){
+      storage[letter]--;
+      counter++;
+    }
+  } 
+
+
+  const diff_of_smaller = (smaller.length - counter)
+  const diff_of_larger = (larger.length - smaller.length)
+
+  return (2 * diff_of_smaller) + diff_of_larger
+
+
+}
